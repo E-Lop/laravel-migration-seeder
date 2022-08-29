0e489a68d987453e8879bc7624509a5fed7d1f04
E-Lop <edmondolopez@gmail.com>
2022-08-29 16:00:06 +0200

    populated seeder

--- database/seeds/TravelsTableSeeder.php
+++ database/seeds/TravelsTableSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Faker\Generator;
+use App\Travel;
+use Faker\Generator as Faker;
 
 class TravelsTableSeeder extends Seeder
 {
@@ -13,15 +14,15 @@ class TravelsTableSeeder extends Seeder
     public function run(Faker $faker)
     {
         // popola la tabella con 10 righe
-        for($i>1; $i<=10; $i++) {
+        for($i=1; $i<=10; $i++) {
             // crea una nuova riga
             $new_travel = new Travel();
             // popola le righe
             $new_travel->reference = $faker->randomNumber(5, true);
-            $new_travel->destination = $faker->city();
+            $new_travel->destinations = $faker->city();
             $new_travel->duration = rand(1, 14);
             $new_travel->price = $faker->randomFloat(2, 300, 2000);
-            $new_travel->visa_needed = rand(0, 1)->default(1);
+            $new_travel->visa_needed = rand(0, 1);
             // salva la riga
             $new_travel->save();
         }
