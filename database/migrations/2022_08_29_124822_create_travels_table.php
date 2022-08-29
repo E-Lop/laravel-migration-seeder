5725d1fd2aebfe5018bce3b8f76c00da251667f5
E-Lop <edmondolopez@gmail.com>
2022-08-29 14:55:11 +0200

    creato migration per tabella travels

---
+++ database/migrations/2022_08_29_124822_create_travels_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            // codice riferimento viaggio
+            $table->string('reference', 12);
+            // destinazione del viaggio
+            $table->string('destinations', 50);
+            // giorni di durata del viaggio
+            $table->smallInteger('duration');
+            // prezzo del viaggio
+            $table->decimal('price', 8, 2);
+            // visto di ingresso necessario
+            $table->tinyInteger('visa_needed')->unsigned()->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
