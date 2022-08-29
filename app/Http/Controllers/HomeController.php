d2e08f285dcfbbc6125943aac2775e59b28fbd9a
E-Lop <edmondolopez@gmail.com>
2022-08-29 15:13:29 +0200

    aggiunto protected al model Travel

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Travel;
 
 class HomeController extends Controller
 {
     public function home() {
-        
+        $travels = Travel::all();
+        dd($travels);
         return view('home');
     }
 }
